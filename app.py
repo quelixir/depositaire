7d0913765e86c19b13b824331ea0c9e3ece1e676
Nick Schlobohm <nickschlobohm@protonmail.ch>
2021-05-09 17:48:09 +1000

    Formatting

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 # init
 import config
 
 app.debug = config.cfg['meta'].getboolean('depositaire_debug')
+
 # API
 import routes.api.v1
+
 
 # default route
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 if __name__ == '__main__':
     app.run()
-
