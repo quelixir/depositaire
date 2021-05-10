eb5dfbd0f099efa2bdfeac95f8cdd32cafce674f
Nick Schlobohm <nickschlobohm@protonmail.ch>
2021-05-10 16:27:03 +1000

    Create `VERSION` constant

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import configparser
+
 cfg = configparser.ConfigParser()
 cfg.read('depositaire.cfg')
+
+import toml
+
+pyproject_toml = toml.load('pyproject.toml')
+VERSION = pyproject_toml['tool']['poetry']['version']
