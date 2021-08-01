a46581cf9fa1d9d2203955b9c5d361ad054fc9c6
Nick Schlobohm <nickschlobohm@protonmail.ch>
2021-08-02 09:20:02 +1000

    chore: add docstrings to test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,2 +1,11 @@
+"""Initialise tests."""
+
+
 def test_genesis():
+    """
+    Ensure that at least one test runs.
+
+    Ensure at least one test (and passes) so that pytest
+    does not exit with error code 5.
+    """
     pass
