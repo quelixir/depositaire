4eec824f8a188325939b101711c89842ee7c7a37
Nick Schlobohm <nickschlobohm@protonmail.ch>
2021-08-02 21:34:48 +1000

    add: SQLite support

--- depositaire/database.py
+++ depositaire/database.py
@@ -1,23 +1,33 @@
 """Connects to the database."""
 
-import mysql.connector as db
+import sqlite3
+
+import mysql.connector as mysql
 
 from depositaire import config
 
-conn = db.connect(
-    user=config.get('DB_USER'),
-    password=config.get('DB_PASS'),
-    host=config.get('DB_HOST'),
-    port=config.getint('DB_PORT'),
-    database=config.get('DB_NAME'),
-)
+if config.get('DB_TYPE').upper() == 'MARIADB':
+    conn = mysql.connect(
+        user=config.get('DB_USER'),
+        password=config.get('DB_PASS'),
+        host=config.get('DB_HOST'),
+        port=config.getint('DB_PORT'),
+        database=config.get('DB_NAME'),
+    )
+else:
+    if config.get('DB_FILE') is not None:
+        filename = config.get('DB_FILE')
+    else:
+        filename = 'depositaire.sqlite'
+
+    conn = sqlite3.connect(filename, check_same_thread=False)
 
 
 def get():
     """
-    Return the database MySQLConnection object.
+    Return the database connection object.
 
     Returns:
-        The database MySQLConnection object.
+        The database connection object.
     """
     return conn
